e4bb248b7b22c23558d4255cfdde930dcfd6c4c3
Dimas Saputra <dragdimas9@gmail.com>
2023-06-08 05:51:37 +0700

    feat(chapter): add comments chapter

---
+++ comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // hello world!
+    println!("Hello, world!"); // this is hello world tho
+
+    // So we’re doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what’s going on.
+}
